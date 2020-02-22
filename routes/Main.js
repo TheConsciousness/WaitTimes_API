1253e6c53813f839e9d579f10e36fd21d8f6c93e
theconsciousness <jordanabrinkman@gmail.com>
2020-02-22 18:49:14 +0000

    updated routes. updated mongo model with new fields.

--- routes/Main.js
+++ routes/Main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const moment = require("moment");
+const moment = require("moment-timezone");
 const router = express.Router();
 const Ride = require("../models/Ride");
 var ObjectID = require("bson-objectid");
@@ -60,10 +60,40 @@ router.get("/days/:date", async (req, res) => {
 });
 
 // Get name between dates
-router.get("/name/:name/between/:dayOne/and/:dayTwo", async (req, res) => {
+router.get("/name/:name/between/days/:dayOne/and/:dayTwo", async (req, res) => {
+
+	console.log(`'/name/${req.params.name}/between/days/${req.params.dayOne}/and/${req.params.dayTwo}' requested`);
+	
+	if (!moment(req.params.dayOne).isValid() || !moment(req.params.dayTwo).isValid()) {
+		return res.send("Input dates not valid");
+	}
+	
+	try {
+		const tmpObjIdOne = ObjectID.createFromTime(moment(req.params.dayOne).tz('America/Indiana/Indianapolis').startOf('day').unix());
+		const tmpObjIdTwo = ObjectID.createFromTime(moment(req.params.dayTwo).tz('America/Indiana/Indianapolis').endOf('day').unix());
+		
+		const rides = await Ride.find({"_id":{$gt: tmpObjIdOne, $lt: tmpObjIdTwo}, "name":req.params.name});
+		const beforeTime = moment().valueOf();
+		
+		//rides.forEach((ride,index) => { console.log(ride); console.log('the id: '+ ride._id); console.log('the name: '+ride.name); });
+				
+		let formattedRides = rides.map((ride) => {
+			//console.log("1: "+ moment(ride.toJSON()._id.getTimestamp()).tz('America/Indiana/Indianapolis').format());
+			return { time:moment(ride.toJSON()._id.getTimestamp()).tz('America/Indiana/Indianapolis').format('MM/DD/YYYY h:mm:ss A'), waitTime:ride.toJSON().waitTime };
+		});
+		const afterTime = moment().valueOf();
+				
+		return res.send(formattedRides); 
+	}
+	catch (err) {
+		return res.send("catch: "+err);
+	}
+});
+
+// Get name between times
+router.get("/name/:name/between/times/:dayOne/and/:dayTwo", async (req, res) => {
 
 	console.log(`'/name/${req.params.name}/between/${req.params.dayOne}/and/${req.params.dayTwo}' requested`);
-	console.log("Using moment of "+moment("2020-02-15").endOf('day').unix());
 	
 		if (!moment(req.params.dayOne).isValid() || !moment(req.params.dayTwo).isValid()) {
 		return res.send("Input dates not valid");
@@ -76,12 +106,15 @@ router.get("/name/:name/between/:dayOne/and/:dayTwo", async (req, res) => {
 		//const tmpObjIdTwo = ObjectID.createFromTime(moment("2020-02-16").endOf('day').unix());
 		
 		const rides = await Ride.find({"_id":{$gt: tmpObjIdOne, $lt: tmpObjIdTwo}, "name":req.params.name});
+		const beforeTime = moment().valueOf();
 		
 		//rides.forEach((ride,index) => { console.log(ride); console.log('the id: '+ ride._id); console.log('the name: '+ride.name); });
 				
 		let formattedRides = rides.map((ride) => {
-			return { id:ride.toJSON()._id, waitTime:ride.toJSON().waitTime };
+			//console.log("1: "+ moment(ride.toJSON()._id.getTimestamp()).tz('America/Indiana/Indianapolis').format());
+			return { time:moment(ride.toJSON()._id.getTimestamp()).tz('America/Indiana/Indianapolis').format('MM/DD/YYYY h:mm:ss A'), waitTime:ride.toJSON().waitTime };
 		});
+		const afterTime = moment().valueOf();
 				
 		return res.send(formattedRides); 
 	}
@@ -90,7 +123,8 @@ router.get("/name/:name/between/:dayOne/and/:dayTwo", async (req, res) => {
 	}
 });
 
-// Get all
+
+// Get one
 router.get("/", async (req, res) => {
 	console.log("'/' requested");
 	
